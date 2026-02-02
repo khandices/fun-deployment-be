b108218d9e058cebfe004455cd3a730f5df00b98
khandices <s.khandice@gmail.com>
2026-02-02 13:21:18 -0500

    KAS: Update to add SQLalchemy class and logic

--- db.py
+++ db.py
@@ -1,7 +1,32 @@
 import os
 from typing import Any
-
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 import psycopg2
+
+load_dotenv()
+Base = declarative_base()
+
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
+DB_PORT = os.getenv("POSTGRES_PORT", "5432")
+DB_NAME = os.getenv("POSTGRES_DB")
+
+if not all([DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME]):
+    raise RuntimeError("Missing required database environment variables")
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(DATABASE_URL)
+LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    session = LocalSession()
+    try:
+        yield session
+    finally:
+        session.close()
 
 
 def get_env(name: str) -> str:
@@ -23,13 +48,14 @@ def get_db_connection():
 
 def read_db(
         query: str,
-        params=None)-> list[tuple[Any, ...]]:
+        params=None)-> list[dict[str, Any]]:
     """
-    Returns: A list of result rows as tuples.
+    Returns: A list of result as list of dictionaries.
     """
     with get_db_connection() as connection:
         with connection.cursor() as cursor:
             cursor.execute(query, params)
+            rows = cursor.fetchall()
             result = cursor.fetchall()
             return result
 
---
+++ models/Job.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Date, Boolean
+from db import Base
+
+class Job(Base):
+    __tablename__ = "jobs"
+
+    id = Column(Integer, primary_key=True)
+    company_name = Column(String, nullable=False)
+    job_title = Column(String, nullable=False)
+    job_type = Column(String, nullable=False)
+    date_applied = Column(Date, nullable=False)
+    location_type = Column(String, nullable=False)
+    referral = Column(Boolean, nullable=False)
+
+    def to_dict(self):
+        return  {column.name: getattr(self, column.name) for column in self.__table__.columns}
